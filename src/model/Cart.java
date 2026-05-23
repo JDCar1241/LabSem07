89f5cdd4f52515b7379b49798b3ec9d9b1ba34a8
JDCar1241 <jcardenasv3.a@upao.edu.pe>
2026-05-23 10:01:53 -0500

    feat: Creacion de Model

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,4 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+
+        for (Product p : products) {
+            total += p.getPrice();
+        }
+
+        return total;
+    }
 }
--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Product {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
